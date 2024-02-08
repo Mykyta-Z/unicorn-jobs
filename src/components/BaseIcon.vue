82a850cf20f0865767d7068f1ecc3eccc852647b
Mykyta Zavdoviev <m.zavdoviev@aimprosoft.com>
2024-02-08 12:22:16 +0100

    Add icons

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: String,
+    default: 's',
+    validator(value) {
+      // Three sizes are available
+      return ['s', 'm', 'l'].includes(value)
+    }
+  }
+})
+</script>
+
+<template>
+  <span class="material-icons" :class="[`size-${size}`]">
+    {{ props.icon }}
+  </span>
+</template>
+
+<style lang="scss" scoped>
+.size-s {
+  font-size: $space;
+}
+.size-m {
+  font-size: $space * 2;
+}
+.size-l {
+  font-size: $space * 4;
+}
+</style>
